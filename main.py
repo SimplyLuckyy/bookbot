add80b01b0d78269088f82fb4f5f9e8ea14f7e44
SimplyLuckyy <gidsgreyman@gmail.com>
2025-06-25 20:39:38 -0500

    added sys arguments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from stats import count_words
 from stats import count_characters
 from stats import sorter
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,18 +10,24 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    word_count = count_words(get_book_text("books/frankenstein.txt"))
-    sorted_list = sorter(count_characters(get_book_text("books/frankenstein.txt")))
-    text_one = f"""
-    ============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
-    ----------- Word Count ----------
-    Found {word_count} total words
-    --------- Character Count -------"""
-    print(text_one)
-    for i in range(0, len(sorted_list)):
-        if sorted_list[i]["char"].isalpha():
-            print(f"{sorted_list[i]["char"]}: {sorted_list[i]["num"]}")
-    print("============= END ===============")
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:    
+        word_count = count_words(get_book_text(sys.argv[1]))
+        sorted_list = sorter(count_characters(get_book_text(sys.argv[1])))
+        text_one = f"""
+        ============ BOOKBOT ============
+        Analyzing book found at {sys.argv[1]}...
+        ----------- Word Count ----------
+        Found {word_count} total words
+        --------- Character Count -------"""
+        
+        print(text_one)
+        for i in range(0, len(sorted_list)):
+            if sorted_list[i]["char"].isalpha():
+                print(f"{sorted_list[i]["char"]}: {sorted_list[i]["num"]}")
+        print("============= END ===============")
 
 main()
